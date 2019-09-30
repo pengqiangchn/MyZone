78073b256e4874a69ec5a133135a1600bb17216b
彭强 <pengqiangchn@foxmail.com>
2019-09-30 23:22:48 +0800

    增加一些文件

--- Startup.cs
+++ Startup.cs
@@ -39,6 +39,7 @@ namespace MyZone
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -52,6 +53,7 @@ namespace MyZone
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
